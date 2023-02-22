9cd4e8187e0e31203f3ea133e46b891cc6e46e6d
KossIOS <kossioskoroban@gmil.com>
2023-02-22 19:43:48 +0100

    Initial Commit

---
+++ DeliveryPizza/DeliveryPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryPizzaApp.swift
+//  DeliveryPizza
+//
+//  Created by Konstantyn Koroban on 22/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
