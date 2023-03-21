dfc81bea8306bb4442c2c1ba56f482ddf58013af
KossIOS <kossioskoroban@gmil.com>
2023-03-21 23:01:29 +0100

    add visual elements

--- DeliveryPizza/ContentView.swift
+++ DeliveryPizza/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        VStack(spacing: 20) {
+            Text("Authorization")
+                .padding()
+                .padding(.horizontal, 30)
+                .font(.title2.bold())
+                .background(Color("whiteAlpha"))
+                .cornerRadius(30)
+                .padding(.top, -45)
+            
+            VStack {
+                TextField("Enter your email", text: $email)
+                    .padding()
+                    .background(Color("whiteAlpha"))
+                    .cornerRadius(15)
+                    .padding(8)
+                    .padding(.horizontal, 12)
+                
+                SecureField("Password", text: $email)
+                    .padding()
+                    .background(Color("whiteAlpha"))
+                    .cornerRadius(15)
+                    .padding(8)
+                    .padding(.horizontal, 12)
+                
+                Button {
+                    print ("Aut")
+                } label: {
+                    Text("Sign In")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(LinearGradient(colors: [Color("yellow"), Color("orange")], startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(15)
+                        .padding(8)
+                        .padding(.horizontal, 12)
+                        .font(.title3.bold())
+                        .foregroundColor(.black)
+                }
+                
+                Button {
+                    print ("Aut")
+                } label: {
+                    Text("Log In")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(LinearGradient(colors: [Color("yellow"), Color("orange")], startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(15)
+                        .padding(8)
+                        .padding(.horizontal, 12)
+                        .font(.title3.bold())
+                        .foregroundColor(.black)
+                }
+            }
+            
+            
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Image("background")).ignoresSafeArea()
     }
 }
 
